a936e161b99669eb5206922957aeb220ab496a9c
namoshizun <dilu3100@gmail.com>
2023-09-17 20:59:55 +0800

    Dockerfile for base, doc and testing separately; Add "test" poetry group

--- tradepy/__init__.py
+++ tradepy/__init__.py
@@ -15,6 +15,10 @@ def is_running_tests():
     return "pytest" in sys.modules
 
 
+def is_building_docs():
+    return "sphinx" in sys.modules
+
+
 tqdm.pandas()
 
 random.seed()
@@ -25,7 +29,7 @@ ts_api = tushare
 try:
     config: TradePyConf = TradePyConf.load_from_config_file()
 except FileNotFoundError:
-    if is_bootstrapping() or is_running_tests():
+    if is_bootstrapping() or is_running_tests() or is_building_docs():
         pass
     else:
         raise
